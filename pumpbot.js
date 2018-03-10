393a30577944e2c1d90a6f4604a9fd5bf4bcbfc4
ToolB0xx <gonzalezgabriel91@gmail.com>
2018-03-10 15:00:44 -0500

    Re-added Terminal Kit, Panic Sell, PNL, etc.
    Terminal Kit dependency added back for colors
    Changed some feedback text for consistency and clarity
    Implemented polling feed function to live track price changes and unrealized PNL
    Implemented functions for averaging the price across a market buy and to calculate percentages
    Implemented panic sell command (Ctrl + S) that will close as a market sell, exiting original position opened

--- pumpbot.js
+++ pumpbot.js
@@ -14,11 +14,15 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+var term = require( 'terminal-kit' ).terminal;
+//let parsedArgs = parseArgs(process.argv.slice(2));
 
 /**********************************
 * VARIABLES
 ***********************************/
 var buyOrderPoll;
+var tQty; // global variable for the quantity filled for an order
+var tFill; //global variable for lastPrice taken from binance websocket
 
 /**********************************
 * INITIALIZATION
@@ -42,7 +46,7 @@ function checkLoadingState() {
 }
 
 function waitForInput() {
-  rl.question(`Input a coin: `, (answer) =>
+  rl.question(`\nInput a coin: `, (answer) =>
   {
     let info = api.getCoinInfo(answer);
     if(info.hasBittrex || info.hasBinance) {
@@ -69,7 +73,7 @@ function waitForInput() {
         buyBinance(info);
       }
     } else {
-      logger.log('info',`coin not found or not on binance/bittrex`);
+      logger.log('info',`coin was not found on binance/bittrex`);
       waitForCoinInput();
     }
   });
@@ -77,10 +81,11 @@ function waitForInput() {
 
 function buyBittrex(info) {
   let price = info.bittrexPrice + (info.bittrexPrice * mainConfig.market_buy_inflation);
-  console.log(bittrexConfig.investment);
+  //console.log(bittrexConfig.investment);
   let shares = bittrexConfig.investment / price;
-  console.log(`buying ${shares} of ${info.coin} at ${price} on bittrex`);
-  api.bittrex.buylimit({market: 'BTC-'+info.coin.toUpperCase(), quantity: shares, rate: price}, (data,err) => {
+  let coin = 'BTC-'+info.coin.toUpperCase();
+  term.brightBlue(`BITTREX: `).defaultColor(`Attempting to buy ${shares} shares of ${info.coin} at `).brightGreen(`B${price}`);
+  api.bittrex.buylimit({market: coin, quantity: shares, rate: price}, (data,err) => {
     if(err) {
       logger.log('error', `Bittrex purchase was unsuccesful: ${err.message}`);
     } else {
@@ -107,19 +112,100 @@ function buyBittrex(info) {
 function buyBinance(info) {
   let price = info.binancePrice + (info.binancePrice * mainConfig.market_buy_inflation);
   let shares = binanceConfig.investment / price;
+  let coin = info.coin.toUpperCase()+"BTC";
+
   shares = convertToCorrectLotSize(shares,info.binanceFormatInfo);
-  console.log(`buying ${shares} of ${info.coin} on binance`);
-  api.binance.marketBuy(info.coin.toUpperCase()+"BTC", shares, function(response) {
-    console.log(response);
-    console.log(`binance buy response: coin: ${response.symbol}, shares: ${response.executedQty}`);
-    // binance.orderStatus("ETHBTC", orderid, (error, orderStatus, symbol) => {
-    //   console.log(symbol+" order status:", orderStatus);
-    // });
+  console.log('');
+  term.brightYellow(`BINANCE: `).defaultColor(`Attempting to buy ${shares} shares of ${info.coin.toUpperCase()} at `).brightGreen(`B${price.toFixed(8)}\n\n`);
+
+  const flags = {type: 'MARKET', newOrderRespType: 'FULL'};
+  api.binance.marketBuy(coin, shares, flags, function(responseBuy) {
+
+    var avgPrice = findAveragePrice(responseBuy);
+
+    term.brightYellow(`BINANCE: `).defaultColor(`Successfully bought ${responseBuy.executedQty} shares of ${responseBuy.symbol} at `).brightGreen(`B${avgPrice}\n\n`);
+
+    tQty = responseBuy.executedQty;
+    let orderID = responseBuy.orderId;
+
+    //checkOrderStatus(coin, orderID);
+    pollProfitAndLoss(coin, avgPrice);
   });
 }
 
-function checkOrderStatus(orderUUID) {
+/*
+function checkOrderStatus(asset, id) {
+  api.binance.orderStatus(asset, id, function(responseStatus) {
+    console.log(responseStatus);
+  });
+}
+*/
 
+function pollProfitAndLoss(asset, fillPrice) {
+
+  api.binance.websockets.chart(asset, "1m", (symbol, interval, chart) => {
+    let tick = api.binance.last(chart);
+    var last = chart[tick].close;
+    tFill = fillPrice;
+
+    avgGain = convertToPercentage(fillPrice, last);
+
+    if(avgGain.indexOf("-") > -1) {
+      term(`Last Price: `).brightRed(`${last}\t`).defaultColor(`Gain: `).brightRed(`${avgGain} %\n`);
+    } else {
+      term(`Last Price: `).brightGreen(`${last}\t`).defaultColor(`Gain: `).brightGreen(` ${avgGain} %\n`);
+    }
+  });
+
+  readline.emitKeypressEvents(process.stdin);
+  process.stdin.setRawMode(true);
+  process.stdin.on('keypress', (str, key) => {
+    if (key.ctrl && key.name === 'c') {
+      exit('\nCtrl + C Detected, Exiting...\n');
+    } else if (key.ctrl && key.name === 's') {
+      inLoop = 0;
+      console.log('\nCtrl + S Detected, Selling...\n');
+      sellBinance(asset, tQty);
+    }
+  });
+}
+
+function sellBinance(asset, quantity) {
+  const flags = {type: 'MARKET', newOrderRespType: 'FULL'};
+  api.binance.marketSell(asset, quantity, flags, function(responseSell) {
+
+    var avgPrice = findAveragePrice(responseSell);
+    var totalProfit = convertToPercentage(avgPrice, tFill);
+
+    term.brightYellow(`BINANCE: `).defaultColor(`Successfully sold ${responseSell.executedQty} shares of ${responseSell.symbol} at `).brightGreen(`B${avgPrice}\n`);
+
+    if(totalProfit.indexOf("-") > -1) {
+      term(`You are now down `).brightRed(`${totalProfit} %`);
+      exit('\n\nYou make some, you lose some');
+    } else {
+      term(`You are now up `).brightGreen(` ${totalProfit} %`);
+      exit('\n\nAnother win for TIC');
+    }
+  });
+}
+
+function sellBinancePart(asset, percent) {
+
+}
+
+function findAveragePrice(array) {
+  var sum = 0;
+  for(var i = 0; i < array.fills.length; i++) {
+      sum += array.fills[i].price;
+  }
+  return sum / array.fills.length;
+}
+
+function convertToPercentage(initial, next) {
+  var x = next - initial;
+  x = x / next * 100;
+  x = x.toFixed(2);
+  return x;
 }
 
 function convertToCorrectLotSize(shares, requirement) {
